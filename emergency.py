12f361056efee162203f4184c83e3527bbf61afc
unknown <esfortnitebr@gmail.com>
2020-07-30 23:08:46 +0200

    fixes

--- emergency.py
+++ emergency.py
@@ -16,25 +16,27 @@ setDelay = 60
 res0 = requests.get(url, headers = headers).json()
 emergency = res0['emergencynotice']['news']['messages']
 
-playlist = emergency['playlistId']
-body = emergency['body']
-title = emergency['title']
-
 while 1:
-    res1 = requests.get(url, headers = headers).json()
-    emergencyNew = res1['emergencynotice']['news']['messages']
-    if emergencyNew != emergency:
-        try:
-            for i in emergencyNew:
-                print('Se ha detectado ' + emergencyNew + i['title'])
-                api.update_with_media('assets/emergency.png', i['title'] + ' (' + i['playlist'] + ')' '\n\n' + i['body'])
-                print('Publicado correctamente.')
-            res2 = requests.get(url, headers = headers).json()
-            emergency = res2['emergencynotice']['news']['messages']
-        except:
-            print('No se ha podido enviar el Tweet.')
-    else:
-        print('No se detectan cambios en Emergency Notice. Buscando de nuevo en 60 segundos.')
+    try:
+        playlist = emergency['playlistId']
+        body = emergency['body']
+        title = emergency['title']
+        res1 = requests.get(url, headers = headers).json()
+        emergencyNew = res1['emergencynotice']['news']['messages']
+        if emergencyNew != emergency:
+            try:
+                for i in emergencyNew:
+                    print('Se ha detectado ' + emergencyNew + i['title'])
+                    api.update_with_media('assets/emergency.png', i['title'] + ' (' + i['playlist'] + ')' '\n\n' + i['body'])
+                    print('Publicado correctamente.')
+                res2 = requests.get(url, headers = headers).json()
+                emergency = res2['emergencynotice']['news']['messages']
+            except:
+                print('No se ha podido enviar el Tweet.')
+        else:
+            print('No se detectan cambios en Emergency Notice. Buscando de nuevo en 60 segundos.')
+    except:
+        print('No hay un aviso de emergencia.')
 
     time.sleep(setDelay)
 
